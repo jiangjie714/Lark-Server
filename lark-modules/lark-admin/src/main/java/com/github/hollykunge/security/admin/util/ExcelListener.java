a59b9dc560ad3cb9f96918a3f0a6f40d7a8a9f6b
fansq-j <13552796829@163.com>
2020-02-25 22:58:52 +0800

    fansq-优化导入导出-20-2-25

--- lark-modules/lark-admin/src/main/java/com/github/hollykunge/security/admin/util/ExcelListener.java
+++ lark-modules/lark-admin/src/main/java/com/github/hollykunge/security/admin/util/ExcelListener.java
@@ -1,10 +1,12 @@
 package com.github.hollykunge.security.admin.util;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
+import com.alibaba.excel.exception.ExcelDataConvertException;
 import com.alibaba.fastjson.JSON;
 import com.github.hollykunge.security.admin.biz.UserBiz;
 import com.github.hollykunge.security.admin.constant.AdminCommonConstant;
@@ -22,6 +24,7 @@ import com.github.hollykunge.security.common.exception.BaseException;
 import com.github.hollykunge.security.common.util.EntityUtils;
 import com.github.hollykunge.security.common.util.SpecialStrUtils;
 import com.github.hollykunge.security.common.util.UUIDUtils;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -66,6 +69,7 @@ public class ExcelListener<T extends  BaseEntity> extends AnalysisEventListener<
 	List<RoleUserMap> roleUserMaps = new ArrayList<RoleUserMap>();
 	List<PositionUserMap> positionUserMaps = new ArrayList<PositionUserMap>();
 
+	@SneakyThrows
 	@Override
 	public void invoke(T t, AnalysisContext context) {
 		if(t instanceof User){
@@ -95,7 +99,7 @@ public class ExcelListener<T extends  BaseEntity> extends AnalysisEventListener<
 			positionUserMapMapper.insertExcelUserRole(positionUserMaps);
 		}
 		if(!CollectionUtils.isEmpty(orgList)){
-			orgMapper.insertExcelOrg(orgList);
+				orgMapper.insertExcelOrg(orgList);
 		}
 		log.info("存储数据库成功！");
 	}
@@ -103,7 +107,18 @@ public class ExcelListener<T extends  BaseEntity> extends AnalysisEventListener<
 	@Override
 	public void onException(Exception exception, AnalysisContext context) throws Exception {
 		log.error("此处可以记录解析异常日志");
-		super.onException(exception, context);
+		// 如果是某一个单元格的转换异常 能获取到具体行号
+		// 如果要获取头的信息 配合invokeHeadMap使用
+		if (exception instanceof ExcelDataConvertException) {
+			Integer columnIndex = ((ExcelDataConvertException) exception).getColumnIndex() + 1;
+			Integer rowIndex = ((ExcelDataConvertException) exception).getRowIndex() + 1;
+			String message = "第" + rowIndex + "行，第" + columnIndex + "列" + "，数据格式有误，请按照模板规定格式填写！";
+			throw new RuntimeException(message);
+		} else if (exception instanceof RuntimeException) {
+			throw exception;
+		} else {
+			super.onException(exception, context);
+		}
 	}
 
 	/**
@@ -112,39 +127,33 @@ public class ExcelListener<T extends  BaseEntity> extends AnalysisEventListener<
 	 * 导入组织数据
 	 * @param org
 	 */
-	public void importOrgExcel(Org org){
+	public void importOrgExcel(Org org) throws Exception{
 		if(StringUtils.isEmpty(org.getParentId())){
-			errMsg= "上级组织编码不可为空！";
-			return;
+			throw new BaseException("上级组织编码不可为空！");
 		}
 		Org orgSelect = new Org();
 		orgSelect.setId(org.getParentId());
 		Org result = orgMapper.selectByPrimaryKey(orgSelect);
 		if(result==null){
-			errMsg= "上级编码填写错误,没有对应的组织名称！";
-			return;
+			throw new BaseException("上级组织编码填写错误,没有对应的组织名称！");
 		}
 		if(StringUtils.isEmpty(org.getOrgName())){
-			errMsg = "组织名称不可为空！";
-			return;
+			throw new BaseException("组织名称不可为空！");
 		}
 		if(org.getOrderId()==null){
-			errMsg = "排序字段不可为空！";
-			return;
+			throw new BaseException("排序字段不可为空！");
 		}
 		if(StringUtils.isEmpty(org.getOrgCode())){
-			errMsg = "组织编码不可为空！";
-			return;
+			throw new BaseException("组织编码不可为空！");
 		}
 		if(org.getOrgLevel()==null){
-			errMsg = "组织层级不可为空！";
-			return;
+			throw new BaseException("组织层级不可为空！");
 		}
 		if(StringUtils.isEmpty(org.getDescription())){
 			org.setDescription("添加方式为数据导入！");
 		}
 		String orgId = UUIDUtils.generateShortUuid();
-		org.setId(orgId);
+		org.setId(org.getOrgCode());
 		org.setDeleted(AdminCommonConstant.ORG_DELETED_CODE);
 		org.setPathCode(result.getPathCode()+org.getOrgCode()+AdminCommonConstant.ORG_PATH_CODE);
 		org.setPathName(result.getPathName()+org.getOrgName()+AdminCommonConstant.ORG_PATH_NAME);
@@ -165,27 +174,42 @@ public class ExcelListener<T extends  BaseEntity> extends AnalysisEventListener<
 	 */
 	public void importUserExcel(User data){
 		String userId = UUIDUtils.generateShortUuid();
-		if (SpecialStrUtils.check(data.getName())) {
-			errMsg= "姓名中不能包含特殊字符!";
-			return;
+		if(StringUtils.isEmpty(data.getName())){
+			throw new BaseException("姓名不可为空！");
 		}
-		Org org = new Org();
-		org.setId(data.getOrgCode());
-		Org orgName = orgMapper.selectOne(org);
-		if(!StringUtils.equals(orgName.getOrgName(),data.getOrgName())){
-			errMsg="组织机构编码和组织机构名称不匹配!";
-			return;
+		if (SpecialStrUtils.check(data.getName())) {
+			throw new BaseException("姓名中不能包含特殊字符!");
+		}
+		if(StringUtils.isEmpty(data.getPId())){
+			throw  new BaseException("身份证号不可为空！");
 		}
 		//校验身份证是否在数据库中存在
 		User user = new User();
 		user.setPId(data.getPId());
 		if (userMapper.selectCount(user) > 0) {
-			errMsg= "身份证已存在!";
-			return;
+			throw new BaseException("身份证号已存在!");
 		}
-		if(NumberUtils.isNumber(data.getSecretLevel())){
-			errMsg="密级应为数字!";
-			return;
+		if(StringUtils.isEmpty(data.getOrgCode())){
+			throw  new BaseException("所属组织机构编码，不可为空！");
+		}
+		if(StringUtils.isEmpty(data.getOrgName())){
+			throw new BaseException("所属机构名称，不可为空！");
+		}
+		Org org = new Org();
+		org.setId(data.getOrgCode());
+		Org orgName = orgMapper.selectOne(org);
+		if(!StringUtils.equals(orgName.getOrgName(),data.getOrgName())){
+			throw new BaseException("组织机构编码和组织机构名称不匹配!");
+		}
+
+		if(!NumberUtils.isNumber(data.getSecretLevel())){
+			throw new BaseException("密级应为数字!");
+		}
+		if(StringUtils.isEmpty(data.getGender())){
+			throw new BaseException("性别不可为空！");
+		}
+		if(data.getOrderId()==null){
+			throw new BaseException("排序字段不可为空！");
 		}
 		String password = new BCryptPasswordEncoder(UserConstant.PW_ENCORDER_SALT).encode(AdminCommonConstant.USER_PASSWORD_DEFAULT);
 		data.setPassword(password);
@@ -202,7 +226,7 @@ public class ExcelListener<T extends  BaseEntity> extends AnalysisEventListener<
 		roleUserMap.setUserId(userId);
 		roleUserMap.setId(UUIDUtils.generateShortUuid());
 		roleUserMaps.add(roleUserMap);
-		//给导入的用户默认一个权限信息  建研究室内群
+		//给导入的用户默认一个权限信息  建研究室内群 0 1 2
 		PositionUserMap positionUserMap = new PositionUserMap();
 		positionUserMap.setUserId(userId);
 		positionUserMap.setPositionId(AdminCommonConstant.USER_POSITION_DEFAULT);
@@ -214,4 +238,18 @@ public class ExcelListener<T extends  BaseEntity> extends AnalysisEventListener<
 			list.clear();
 		}
 	}
+
+    /**
+     * 是否为数字
+     * @param str
+     * @return
+     */
+    public static boolean isNumeric(String str){
+        for (int i = str.length();--i>=0;){
+            if (!Character.isDigit(str.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
 }
