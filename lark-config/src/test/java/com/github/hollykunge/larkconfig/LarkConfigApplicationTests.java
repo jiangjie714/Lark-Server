79a17746be6929f4a63b346abb2420e3dd8c1220
dodo <hollykunge@gmail.com>
2020-03-16 13:21:08 +0800

    增加配置中心

---
+++ lark-config/src/test/java/com/github/hollykunge/larkconfig/LarkConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hollykunge.larkconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LarkConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
