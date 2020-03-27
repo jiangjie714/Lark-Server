c8d8b7eb0829cb37731803b75b3490b69b12a325
biubiubiu <386845154@qq.com>
2020-03-27 10:46:52 +0800

    增加统计接口 //消息统计 date：jinri benzhou benyue quanbu  今天 本周 本月 全部（可以为空） http://127.0.0.1:8089/statistics/msgStatistics?orgCode=0010&date=quanbu //群统计 date：jinri benzhou benyue quanbu http://127.0.0.1:8089/statistics/groupInfStatistics?orgCode=0010&date=quanbu //附件统计 date：jinri benzhou benyue quanbu http://127.0.0.1:8089/statistics/fileStatistics?orgCode=0010&date=quanbu

--- service-chat/src/main/java/com/workhub/z/servicechat/service/impl/StatisticsServiceImpl.java
+++ service-chat/src/main/java/com/workhub/z/servicechat/service/impl/StatisticsServiceImpl.java
@@ -1,15 +1,13 @@
 package com.workhub.z.servicechat.service.impl;
 
-import com.github.hollykunge.security.common.msg.TableResultResponse;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
-import com.workhub.z.servicechat.VO.StatisticsGroupVo;
-import com.workhub.z.servicechat.VO.StatisticsMsgVo;
+import com.workhub.z.servicechat.VO.StatisticsChartDataVo;
 import com.workhub.z.servicechat.dao.StatisticsDao;
+import com.workhub.z.servicechat.model.StatisticsChartDto;
 import com.workhub.z.servicechat.service.StatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,12 +22,21 @@ public class StatisticsServiceImpl implements StatisticsService {
 
     /**
      * 群统计
-     * @param groupName 群名称
-     * @param isCross 跨场所
      * @return
      */
     @Override
-    public TableResultResponse<StatisticsGroupVo> groupStatistics(int page, int size, String groupName, String isCross) {
+    public  List<StatisticsChartDataVo> groupInfStatistics(String dateType, String orgCode){
+        //获取横坐标
+        List<StatisticsChartDto> statisticsXLabelVos = this.statisticsDao.getXLabel(orgCode);
+        //计算每个点的值
+        for(StatisticsChartDto vo:statisticsXLabelVos){
+            String data = this.statisticsDao.groupInfStatistics(changeDateType(dateType),vo.getId());
+            vo.setCnt(data);
+        }
+        return  changeChartData(statisticsXLabelVos);
+    }
+
+    /*public TableResultResponse<StatisticsGroupVo> groupStatistics(int page,int size,String groupName, String isCross) {
         PageHelper.startPage(page, size);
         List<StatisticsGroupVo> dataList =this.statisticsDao.groupStatistics(groupName,isCross);
         PageInfo<StatisticsGroupVo> pageInfo = new PageInfo<>(dataList);
@@ -41,14 +48,80 @@ public class StatisticsServiceImpl implements StatisticsService {
                 pageInfo.getList()
         );
         return res;
-    }
+    }*/
 
     /**
      * 消息统计
+     * dateType 0全部1天2本周3本月
      * @return
      */
     @Override
-    public StatisticsMsgVo msgStatistics() {
-        return this.statisticsDao.msgStatistics();
+    public  List<StatisticsChartDataVo> msgStatistics(String dateType, String orgCode){
+        //获取横坐标
+        List<StatisticsChartDto> statisticsXLabelVos = this.statisticsDao.getXLabel(orgCode);
+        //计算每个点的值
+        for(StatisticsChartDto vo:statisticsXLabelVos){
+            String data = this.statisticsDao.msgStatistics(changeDateType(dateType),vo.getId());
+            vo.setCnt(data);
+        }
+        return  changeChartData(statisticsXLabelVos);
+    }
+
+    /*public StatisticsMsgVo msgStatistics(String dateType) {
+        return this.statisticsDao.msgStatistics(dateType);
+    }*/
+    /**
+     * 附件统计
+     * @return
+     * dateType 0全部1天2周3年
+     */
+    @Override
+    public  List<StatisticsChartDataVo> fileStatistics(String dateType, String orgCode){
+        //获取横坐标
+        List<StatisticsChartDto> statisticsXLabelVos = this.statisticsDao.getXLabel(orgCode);
+        //计算每个点的值
+        for(StatisticsChartDto vo:statisticsXLabelVos){
+            String data = this.statisticsDao.fileStatistics(changeDateType(dateType),vo.getId());
+            vo.setCnt(data);
+        }
+        return  changeChartData(statisticsXLabelVos);
+    }
+    /*public StatisticsFileVo fileStatistics(String dateType) {
+        return this.statisticsDao.fileStatistics(dateType);
+    }*/
+
+    /**
+     * 数据转换为chat需要的格式
+     * @param oriDatas
+     * @return
+     */
+    private List<StatisticsChartDataVo> changeChartData(List<StatisticsChartDto> oriDatas){
+        List<StatisticsChartDataVo> res = new ArrayList<>(16);
+        for(StatisticsChartDto oriData:oriDatas){
+            StatisticsChartDataVo vo = new StatisticsChartDataVo();
+            vo.setX(oriData.getOrgName());
+            vo.setY(Long.valueOf(oriData.getCnt()));
+            res.add(vo);
+        }
+        return res;
+    }
+
+    /**
+     * 日期查询转码
+     * @param date
+     * @return
+     */
+    private String changeDateType(String date){
+        if(date==null || "".equals(date) || "quanbu".equals(date)){
+            return "0";
+        }else if("jinri".equals(date)){
+            return "1";
+        }else if("benzhou".equals(date)){
+            return "2";
+        }else if("benyue".equals(date)){
+            return "3";
+        }else {
+            return "0";
+        }
     }
 }
