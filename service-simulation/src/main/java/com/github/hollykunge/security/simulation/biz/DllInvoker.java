5945469ff60386aad5b0cf209df1885fdc535864
fresh-ji <jihangchn@163.com>
2020-04-11 15:02:45 +0800

    当前外网配置2

--- service-simulation/src/main/java/com/github/hollykunge/security/simulation/biz/DllInvoker.java
+++ service-simulation/src/main/java/com/github/hollykunge/security/simulation/biz/DllInvoker.java
@@ -6,7 +6,7 @@ import com.sun.jna.Native;
 public interface DllInvoker extends Library {
 
     DllInvoker instance = (DllInvoker) Native.loadLibrary(
-            "D:\\WorkSpace\\OpenSplice\\OSPL\\spliceForEngine.dll", DllInvoker.class);
+            "D:\\ospl\\spliceForEngine.dll", DllInvoker.class);
 
     String dllSystemPrepare(String systemName, double startTime, double step, String users);
 
