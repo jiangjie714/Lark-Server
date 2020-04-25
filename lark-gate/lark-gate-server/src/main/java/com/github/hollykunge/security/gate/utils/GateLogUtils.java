7c57cb48d766418c0ac06276cd19d0ae08280142
zhoyou <18525788842@163.com>
2020-04-25 20:52:32 +0800

    统一gate异常响应编码

--- lark-gate/lark-gate-server/src/main/java/com/github/hollykunge/security/gate/utils/GateLogUtils.java
+++ lark-gate/lark-gate-server/src/main/java/com/github/hollykunge/security/gate/utils/GateLogUtils.java
@@ -2,9 +2,7 @@ package com.github.hollykunge.security.gate.utils;
 
 import com.alibaba.fastjson.JSONObject;
 import com.github.hollykunge.security.common.constant.CommonConstants;
-import com.github.hollykunge.security.common.exception.BaseException;
-import com.github.hollykunge.security.common.exception.auth.ClientInvalidException;
-import com.github.hollykunge.security.common.msg.ObjectRestResponse;
+import com.github.hollykunge.security.common.exception.server.ServerInvalidException;
 import com.netflix.zuul.context.RequestContext;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
@@ -125,7 +123,7 @@ public class GateLogUtils {
         }
         String[] nameArray = CommonConstants.GET_GATE_LOG_REQUEST_BODY_RULE.split(",");
         if (nameArray.length == 0) {
-            throw new BaseException("获取requestbody实体类基本参数规则没有在常量池中定义...");
+            throw new ServerInvalidException("获取requestbody实体类基本参数规则没有在常量池中定义...");
         }
         String result = "";
         for (String name :
@@ -140,13 +138,13 @@ public class GateLogUtils {
 
     private String checkFunctionParam(RequestContext ctx) {
         if (ctx == null) {
-            throw new ClientInvalidException("request must be not null...");
+            throw new ServerInvalidException("request must be not null...");
         }
         String requestURI = ctx.getRequest().getRequestURI();
         requestURI = requestURI.substring(zuulPrefix.length() + 1, requestURI.length());
         requestURI = requestURI.substring(requestURI.indexOf("/"), requestURI.length());
         if (StringUtils.isEmpty(requestURI)) {
-            throw new ClientInvalidException("request no request uri .. ");
+            throw new ServerInvalidException("request no request uri .. ");
         }
         return requestURI;
     }
