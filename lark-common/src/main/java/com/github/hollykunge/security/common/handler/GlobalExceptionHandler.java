d09c992da1ebccff94c2432b6d888d06794d2a0a
zhoyou <18525788842@163.com>
2020-04-25 20:38:40 +0800

    全局统一异常，返回规范的异常状态码

--- lark-common/src/main/java/com/github/hollykunge/security/common/handler/GlobalExceptionHandler.java
+++ lark-common/src/main/java/com/github/hollykunge/security/common/handler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.github.hollykunge.security.common.constant.CommonConstants;
 import com.github.hollykunge.security.common.dictionary.HttpReponseStatusEnum;
 import com.github.hollykunge.security.common.exception.BaseException;
 import com.github.hollykunge.security.common.exception.BizException;
+import com.github.hollykunge.security.common.exception.auth.ClientForbiddenException;
 import com.github.hollykunge.security.common.exception.auth.ClientTokenException;
 import com.github.hollykunge.security.common.exception.auth.UserTokenException;
 import com.github.hollykunge.security.common.msg.BaseResponse;
@@ -27,11 +28,30 @@ import javax.servlet.http.HttpServletResponse;
 @ResponseBody
 public class GlobalExceptionHandler {
     private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    /**
+     * 服务器运行时异常
+     * @param response
+     * @param ex
+     * @return
+     */
     @ExceptionHandler(BaseException.class)
     public BaseResponse baseExceptionHandler(HttpServletResponse response, BaseException ex) {
         logger.error(ex.getMessage(),ex);
         response.setStatus(HttpReponseStatusEnum.SYSTEM_ERROR.value());
         return new BaseResponse(ex.getStatus(), ex.getMessage());
+    }
+    /**
+     * 特殊处理服务异常中内部服务调用无权限被拒绝异常处理
+     * @param response
+     * @param forBiddenEx
+     * @return
+     */
+    @ExceptionHandler(ClientForbiddenException.class)
+    public BaseResponse clientforbidden(HttpServletResponse response, ClientForbiddenException forBiddenEx){
+        response.setStatus(HttpReponseStatusEnum.RPC_CLIENT_FORBIDDEN.value());
+        logger.error(forBiddenEx.getMessage(),forBiddenEx);
+        return new BaseResponse(forBiddenEx.getStatus(), forBiddenEx.getMessage());
     }
 
     /**
@@ -46,34 +66,9 @@ public class GlobalExceptionHandler {
         logger.error(ex.getMessage(),ex);
         return new BaseResponse(CommonConstants.EX_OTHER_CODE, ex.getMessage());
     }
-    /**
-     * clienttoken验证异常
-     * @param response
-     * @param ex
-     * @return
-     */
-    @ExceptionHandler(ClientTokenException.class)
-    public BaseResponse clientTokenExceptionHandler(HttpServletResponse response, ClientTokenException ex) {
-        response.setStatus(403);
-        logger.error(ex.getMessage(),ex);
-        return new BaseResponse(ex.getStatus(), ex.getMessage());
-    }
-
-    /**
-     * usertoken验证异常
-     * @param response
-     * @param ex
-     * @return
-     */
-    @ExceptionHandler(UserTokenException.class)
-    public BaseResponse userTokenExceptionHandler(HttpServletResponse response, UserTokenException ex) {
-        response.setStatus(401);
-        logger.error(ex.getMessage(),ex);
-        return new BaseResponse(ex.getStatus(), ex.getMessage());
-    }
     //以下为业务异常
     /**
-     * 和spring集成，表单参数校验异常
+     * 特殊处理业务异常中与spring集成，表单参数校验异常
      * @param response 响应
      * @param argumentValidEx 异常封装的实体
      * @return
@@ -98,4 +93,5 @@ public class GlobalExceptionHandler {
         logger.error(bizEx.getMessage(),bizEx);
         return new BaseResponse(bizEx.getStatus(), bizEx.getMessage());
     }
+
 }
