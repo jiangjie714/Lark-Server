67029be8909d5e470d145366256b7ad82a4f6cb6
zhoyou <18525788842@163.com>
2020-04-28 11:16:21 +0800

    log日志转到了es中存储

--- lark-modules/lark-admin-server-api/src/main/java/com/github/hollykunge/security/admin/api/service/AdminLogServiceFeignClient.java
+++ lark-modules/lark-admin-server-api/src/main/java/com/github/hollykunge/security/admin/api/service/AdminLogServiceFeignClient.java
@@ -1,13 +1,13 @@
-package com.github.hollykunge.security.admin.api.service;
-
-import com.github.hollykunge.security.admin.api.hystrix.AdminLogServiceFallback;
-import com.github.hollykunge.security.admin.api.log.LogInfo;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-@FeignClient(value = "lark-admin",fallback = AdminLogServiceFallback.class)
-public interface AdminLogServiceFeignClient {
-  @RequestMapping(value="/api/log/save",method = RequestMethod.POST)
-  void saveLog(LogInfo info);
-}
+//package com.github.hollykunge.security.admin.api.service;
+//
+//import com.github.hollykunge.security.admin.api.hystrix.AdminLogServiceFallback;
+//import com.github.hollykunge.security.admin.api.log.LogInfo;
+//import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//
+//@FeignClient(value = "lark-admin",fallback = AdminLogServiceFallback.class)
+//public interface AdminLogServiceFeignClient {
+//  @RequestMapping(value="/api/log/save",method = RequestMethod.POST)
+//  void saveLog(LogInfo info);
+//}
