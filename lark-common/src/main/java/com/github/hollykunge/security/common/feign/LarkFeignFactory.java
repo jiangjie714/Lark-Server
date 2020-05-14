6fbd47eceb6434766dfcdd566bd781ba2d96f390
zhoyou <18525788842@163.com>
2020-05-14 15:57:07 +0800

    common中增加feign动态代理

---
+++ lark-common/src/main/java/com/github/hollykunge/security/common/feign/LarkFeignFactory.java
@@ -0,0 +1,39 @@
+package com.github.hollykunge.security.common.feign;
+
+import com.github.hollykunge.security.common.exception.server.FeignExecutException;
+import com.github.hollykunge.security.common.msg.FeignResponse;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.net.URLDecoder;
+
+/**
+ * @author: zhhongyu
+ * @description: lark-feign单例工厂
+ * @since: Create in 15:19 2020/5/14
+ */
+public class LarkFeignFactory {
+    private static LarkFeignFactory ourInstance = new LarkFeignFactory();
+
+    public static LarkFeignFactory getInstance() {
+        return ourInstance;
+    }
+
+    private LarkFeignFactory() {
+    }
+
+    public <T> T loadFeign(Object feignService) {
+        Class<?> clazz = feignService.getClass();
+        return (T)Proxy.newProxyInstance(clazz.getClassLoader(),clazz.getInterfaces(),(proxy,method,args)->{
+            Object response = method.invoke(feignService, args);
+            if(response instanceof FeignResponse){
+                FeignResponse feignResponse = (FeignResponse) response;
+                if(feignResponse.getStatus() == 500){
+                    throw new FeignExecutException(feignResponse.getMessage());
+                }
+            }
+            return response;
+        });
+    }
+}
