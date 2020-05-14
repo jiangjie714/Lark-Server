44e204c59a3465f5d34373c8e3430cbde5d590c3
zhoyou <18525788842@163.com>
2020-05-14 09:52:17 +0800

    增加事件监听

--- lark-common/src/main/java/com/github/hollykunge/security/common/aop/FeignInvokeHandler.java
+++ lark-common/src/main/java/com/github/hollykunge/security/common/aop/FeignInvokeHandler.java
@@ -1,11 +1,12 @@
 package com.github.hollykunge.security.common.aop;
 
 
-import com.github.hollykunge.security.common.dictionary.HttpReponseStatusEnum;
-import com.github.hollykunge.security.common.exception.server.FeignExecutException;
+import com.github.hollykunge.security.common.feign.FeignResponseEvent;
 import com.github.hollykunge.security.common.msg.FeignResponse;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 
 /**
  * @author: zhhongyu
@@ -13,15 +14,18 @@ import org.aopalliance.intercept.MethodInvocation;
  * @since: Create in 9:13 2020/5/13
  */
 public class FeignInvokeHandler implements MethodInterceptor {
+    private final ApplicationContext applicationContext;
+    public FeignInvokeHandler(ApplicationContext applicationContext){
+        this.applicationContext = applicationContext;
+    }
+
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
         Object feignResult = invocation.proceed();
         if(feignResult instanceof FeignResponse){
             FeignResponse response = (FeignResponse) feignResult;
-            int status = response.getStatus();
-            if(status == HttpReponseStatusEnum.SYSTEM_ERROR.value()){
-                throw new FeignExecutException(response.getMessage());
-            }
+            FeignResponseEvent event = new FeignResponseEvent(this,response);
+            applicationContext.publishEvent(event);
         }
         return feignResult;
     }
--- lark-common/src/main/java/com/github/hollykunge/security/common/config/FeignConfigration.java
+++ lark-common/src/main/java/com/github/hollykunge/security/common/config/FeignConfigration.java
@@ -2,10 +2,12 @@ package com.github.hollykunge.security.common.config;
 
 import com.github.hollykunge.security.common.aop.FeignInvokeHandler;
 import com.github.hollykunge.security.common.exception.BaseException;
+import com.github.hollykunge.security.common.feign.FeignResponseListener;
 import com.github.hollykunge.security.common.util.Assert;
 import org.springframework.aop.aspectj.AspectJExpressionPointcutAdvisor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -19,13 +21,20 @@ public class FeignConfigration {
 
     @Bean
     @ConditionalOnProperty(prefix = "feign.hystrix",name = "enabled",havingValue = "true")
-    public AspectJExpressionPointcutAdvisor configurabledvisor() {
+    public AspectJExpressionPointcutAdvisor configurabledvisor(ApplicationContext applicationContext) {
         if(Assert.strNull(pointcut)){
             throw new BaseException("系统必须配置feign所在的位置，请配置 feign.aop 属性");
         }
         AspectJExpressionPointcutAdvisor advisor = new AspectJExpressionPointcutAdvisor();
         advisor.setExpression(pointcut);
-        advisor.setAdvice(new FeignInvokeHandler());
+        advisor.setAdvice(new FeignInvokeHandler(applicationContext));
         return advisor;
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "feign.hystrix",name = "enabled",havingValue = "true")
+    public FeignResponseListener feignResponseListener() {
+        FeignResponseListener feignResponseListener = new FeignResponseListener();
+        return feignResponseListener;
     }
 }
