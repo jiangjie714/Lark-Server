a34d2b189b14764326fd0d110acaa360f4cccaca
zhoyou <18525788842@163.com>
2020-05-14 14:43:52 +0800

    修改basehystrix名称

--- lark-auth/lark-auth-server/src/main/java/com/github/hollykunge/security/auth/feign/hystrix/IUserHystrix.java
+++ lark-auth/lark-auth-server/src/main/java/com/github/hollykunge/security/auth/feign/hystrix/IUserHystrix.java
@@ -2,7 +2,7 @@ package com.github.hollykunge.security.auth.feign.hystrix;
 
 import com.github.hollykunge.security.admin.api.dto.AdminUser;
 import com.github.hollykunge.security.auth.feign.IUserService;
-import com.github.hollykunge.security.common.feign.BaseFeignFactory;
+import com.github.hollykunge.security.common.feign.BaseHystrixFactory;
 import com.github.hollykunge.security.common.msg.FeignObjectReponse;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
  * @since: Create in 14:43 2020/5/13
  */
 @Component
-public class IUserHystrix extends BaseFeignFactory<IUserHystrix> implements IUserService {
+public class IUserHystrix extends BaseHystrixFactory<IUserHystrix> implements IUserService {
 
     @Override
     public FeignObjectReponse<AdminUser> validate(String pid, String password) {
